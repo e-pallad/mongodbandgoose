06c2926e9382a2f7a48fa455bab371ccdf9943b4
Eric Pallad <e.pallad@pp-itservices.de>
2022-01-04 16:27:34 +0100

    added mongodb schema

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 var mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Person = new Schema({
+  name:  String,
+  age: Number,
+  favoriteFoods: [{ body: String, date: Date }],
+});
 
 let Person;
 
